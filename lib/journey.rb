c65d5590d69075c922b9d06c60ea3a2d843508f9
Linus Mjornstedt <carl.mjornstedt@gmail.com>
2020-09-09 17:17:50 +0100

    removing journey elements from oystercard

--- lib/journey.rb
+++ lib/journey.rb
@@ -3,7 +3,7 @@ class Journey
   MINIMUM_FARE = 1
   PENALTY_FARE = 6
 
-  attr_reader :entry_station, :exit_station
+  attr_accessor :entry_station, :exit_station
 
   def initialize(entry_station = nil)
     @entry_station = entry_station
@@ -20,6 +20,10 @@ class Journey
 
   def complete?
     @entry_station != nil && @exit_station != nil ? true : false
+  end
+
+  def save_journey
+    {@entry_station => @exit_station}
   end
 
 private
