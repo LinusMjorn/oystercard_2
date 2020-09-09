3dd8ba099e329aa32b677ae9aae43a8010bdfba3
samcolson4 <samcolson4@gmail.com>
2020-09-09 16:45:04 +0100

    Added full journey class tests & passed them

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,5 +1,8 @@
 class Journey
 
+  MINIMUM_FARE = 1
+  PENALTY_FARE = 6
+
   attr_reader :entry_station, :exit_station
 
   def initialize(entry_station = nil)
@@ -9,6 +12,14 @@ class Journey
 
   def finish(station)
     @exit_station = station
+  end
+
+  def fare
+    complete? ? MINIMUM_FARE : PENALTY_FARE
+  end
+
+  def complete?
+    @entry_station != nil && @exit_station != nil ? true : false
   end
 
 private
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,6 +2,7 @@ require 'journey'
 require 'oystercard'
 
 describe Journey do
+
   let(:station) { double :station, zone: 1}
 
   it 'initializes with an optional entry station' do
@@ -18,13 +19,50 @@ describe Journey do
     end
   end
 
-  describe '#fare' do
+  describe '#fare - with exit station' do
+    before do
+      subject.finish(station)
+    end
+  
+    it "should respond to the #fare method" do
+      expect(subject).to respond_to(:fare)
+    end
 
+    it "should return the minimum fare if there is an exit && entry station" do
+      journey = Journey.new("station1")
+      journey.finish(station)
+      expect(journey.fare).to eq Journey::MINIMUM_FARE
+    end
+  end
 
+  describe '#fare - without either entry or exit station' do
+    it "should return PENALTY_FARE [6] if no entry or exit station set" do
+      journey = Journey.new()
+      expect(journey.fare).to eq Journey::PENALTY_FARE
+    end
+
+    it "should return PENALTY fare if only one station (entry)" do
+      journey = Journey.new("station1")
+      expect(journey.fare).to eq Journey::PENALTY_FARE
+    end
+
+    it "should return PENALTY fare if only one station (exit)" do
+      journey = Journey.new()
+      journey.finish("station1")
+      expect(journey.fare).to eq Journey::PENALTY_FARE
+    end
   end
 
   describe '#complete?' do
+     it "should return true if a journey has both entry and exit stations" do
+      journey = Journey.new("station1")
+      journey.finish(station)
+      expect(journey.complete?).to eq true
+    end
 
+    it "should return false if a journey has no entry or exit station" do
+    expect(subject.complete?).to eq false
+    end
   end
 
 end
